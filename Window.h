25ef66813fa3d6f4ad000c9e0cb3e5e0deb38128
Octavio Gonzalez <octavio_ga@hotmail.com>
2022-04-09 23:02:36 -0500

    Inicial files

---
+++ Window.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getCarMove() { return carMove; }
+	GLfloat getChopperX() { return chopperX; }
+	GLfloat getChopperY() { return chopperY; }
+	GLfloat getChopperZ() { return chopperZ; }
+	GLfloat getWarthogX() { return warthogX; }
+	GLfloat getWarthogZ() { return warthogZ; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	GLfloat carMove, warthogX, warthogZ, chopperX, chopperY, chopperZ;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
